ea139d954cfa7a04d45aead10e64cee5bde00e96
Daniel List <daniil.listikov@gmail.com>
2022-04-02 19:46:16 +0300

    Kommit

---
+++ orm.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__) 
+@app.route('/')
+if __name__ == '__main__':
+    app.run(debug=True)
+
+def 
+
+
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sitedb.db' 
+db = SQLAlchemy(app) 
